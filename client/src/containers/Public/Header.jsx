a09001239cd14c45447bb1ff3dd97d97bee2e00d
Mark Son <63505302+sonthuol@users.noreply.github.com>
2023-06-21 21:55:44 +0700

    add toggle state login and register

--- client/src/containers/Public/Header.jsx
+++ client/src/containers/Public/Header.jsx
@@ -10,9 +10,12 @@ const { AiOutlinePlusCircle } = icons;
 const Header = () => {
   const navigate = useNavigate();
 
-  const goLogin = useCallback(() => {
-    navigate(path.LOGIN);
-  }, [navigate]);
+  const goLogin = useCallback(
+    (flag) => {
+      navigate(path.LOGIN, { state: { flag } });
+    },
+    [navigate]
+  );
 
   return (
     <div className="w-1100">
@@ -29,12 +32,13 @@ const Header = () => {
             text="Đăng nhập"
             textColor="text-white"
             bgColor="bg-[#3961FB]"
-            onClick={goLogin}
+            onClick={() => goLogin(false)}
           />
           <Button
             text="Đăng ký"
             textColor="text-white"
             bgColor="bg-[#3961FB]"
+            onClick={() => goLogin(true)}
           />
           <Button
             text="Đăng tin mới"
--- client/src/containers/Public/Login.jsx
+++ client/src/containers/Public/Login.jsx
@@ -1,27 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, InputForm } from "../../components";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const Login = () => {
+  const location = useLocation();
+  const [isRegister, setIsRegister] = useState(location.state?.flag);
+
+  useEffect(() => {
+    setIsRegister(location.state?.flag);
+  }, [location.state?.flag]);
+
+  console.log(location);
   return (
     <div className="bg-white w-[600px] p-[30px] bt-[100px] rounded-md shadow-sm">
-      <h3 className="font-semibold text-2xl mb-3">Đăng nhập</h3>
+      <h3 className="font-semibold text-2xl mb-3">
+        {isRegister ? "Đăng ký" : "Đăng nhập"}
+      </h3>
       <div className="w-full flex flex-col gap-5">
+        {isRegister && <InputForm id="name" lable="HỌ VÀ TÊN " />}
         <InputForm id="phone" lable="SỐ ĐIỆN THOẠI" />
         <InputForm id="password" lable="MẬT KHẨU" />
         <Button
-          text="Đăng nhập"
+          text={isRegister ? "Đăng ký" : "Đăng nhập"}
           textColor="text-white"
           bgColor="bg-secondary1"
           fullWidth
         />
       </div>
-      <div className="flex items-center justify-between mt-7">
-        <span className="text-[blue] hover:text-[red] cursor-pointer">
-          Bạn quên mật khẩu?
-        </span>
-        <span className="text-[blue] hover:text-[red] cursor-pointer">
-          Tạo một tại khoản mới
-        </span>
+      <div
+        className={`flex items-center justify-between mt-7 ${
+          isRegister && "float-right"
+        }`}
+      >
+        {isRegister ? (
+          <small>
+            Bạn đã có tài khoản ?{" "}
+            <span
+              className="text-[blue] hover:text-[red] cursor-pointer"
+              onClick={() => setIsRegister(false)}
+            >
+              Đăng nhập ngay
+            </span>
+          </small>
+        ) : (
+          <>
+            <small className="text-[blue] hover:text-[red] cursor-pointer">
+              Bạn quên mật khẩu?
+            </small>
+            <small
+              className="text-[blue] hover:text-[red] cursor-pointer"
+              onClick={() => setIsRegister(true)}
+            >
+              Tạo một tại khoản mới
+            </small>
+          </>
+        )}
       </div>
     </div>
   );
